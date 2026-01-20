9d74df25b6716fa5f6347430fb6c7d13ea193673
averageMTGplayer <twang6440@student.pps.net>
2026-01-20 13:22:40 -0800

    voice seek code

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,6 +1,12 @@
-from audioFuncs import *
+import pyttsx3
+
 engine = pyttsx3.init()
+voices = engine.getProperty('voices')
 
-print("commecning test")
-
-compSpeak(engine,"hello")
+for voice in voices:
+    print(f"ID: {voice.id}")
+    print(f"Name: {voice.name}")
+    print(f"Languages: {voice.languages}")
+    print(f"Gender: {voice.gender}")
+    print(f"Age: {voice.age}")
+    print("-" * 50)
