1f7ae8c36e8484dc58e9fd6e2ed8ef1b26ef6ad5
averageMTGplayer <twang6440@student.pps.net>
2026-01-22 14:20:57 -0800

    print on test

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,5 +1,6 @@
 from audioFuncs import *
 import pyttsx3
+print("Testing Commencing")
 engine = pyttsx3.init()
 compSpeak(engine, "This is my first time speaking")
 compSpeak(engine, "This is my second time speaking")
