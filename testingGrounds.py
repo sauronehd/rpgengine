a5119b63b94063884fb7736eccbb069d3ec127a3
averageMTGplayer <twang6440@student.pps.net>
2026-02-11 14:35:46 -0800

    minor error fix

--- testingGrounds.py
+++ testingGrounds.py
@@ -4,7 +4,7 @@ from pinCalls import *
 
 GPIO.setmode(GPIO.BOARD)
 pinOutSet()
-outputCall([[pins.cold,pinState.on],[pins.heat,pinState.on],[pins.fan,pinState.on]],[modpins.mister,modState.on])
+outputCall([],[modpins.mister,modState.on])
 wait = input("Press enter to continue")
 pinReset()
 GPIO.cleanup()
