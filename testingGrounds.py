4caf93287a8f428e01e71ddfe23bea6a9f8e19ed
averageMTGplayer <twang6440@student.pps.net>
2026-02-12 12:53:09 -0800

    debugin attempt

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,9 +1,11 @@
 
 from audioFuncs import *
 from pinCalls import *
-
+print("setting to baord")
 GPIO.setmode(GPIO.BOARD)
+print("pinoutsetcall")
 pinOutSet()
+print("output call ")
 outputCall([],[[modpins.mister,modState.on]])
 wait = input("Press enter to continue")
 pinReset()
