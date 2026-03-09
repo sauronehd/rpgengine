be7f4cc0179741870fee58cbce0310d3cf186425
averageMTGplayer <152660249+sauronehd@users.noreply.github.com>
2026-03-09 15:00:14 -0700

    why

--- testingGrounds.py
+++ testingGrounds.py
@@ -9,7 +9,7 @@ print("Text is:"+record_transcribe(model))
 
 
 engine = pyttsx3.init()
-engine.setProperty('voice', "en")
+
 engine.setProperty('rate', 125)
 compSpeak(engine,"") # this clear some lag
 compSpeak(engine,"Hello")
