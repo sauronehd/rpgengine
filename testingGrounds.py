52b5b2239e0626dc16bcfb3dfb2659b029cfc21f
averageMTGplayer <152660249+sauronehd@users.noreply.github.com>
2026-03-09 14:49:00 -0700

    more debuggin

--- testingGrounds.py
+++ testingGrounds.py
@@ -9,8 +9,6 @@ print("Text is:"+record_transcribe(model))
 
 
 engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-print(voices)  # See what's available
 engine.setProperty('voice', "en")
 engine.setProperty('rate', 125)
 compSpeak(engine,"") # this clear some lag
