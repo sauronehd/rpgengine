f17e521d8b9a9ca1a35e3c12a3e5d699c0518eac
averageMTGplayer <152660249+sauronehd@users.noreply.github.com>
2026-03-09 14:14:39 -0700

    testing is a record_transcribe test

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,15 +1,10 @@
-
 from audioFuncs import *
-from pinCalls import *
-print("setting to baord")
-import RPi.GPIO as GPIO
-print("pinoutsetcall")
-pinOutSet()
-print("output call ")
-outputCall([])
-wait = input("Press enter to continue")
-pinReset()
-GPIO.cleanup()
+
+import whisper
+
+model = whisper.loadModel("base")
+
+record_transcribe(model)
 
 
 
