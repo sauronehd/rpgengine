aef7d85686b7cc96d8dccf916393cc3b10df0264
averageMTGplayer <152660249+sauronehd@users.noreply.github.com>
2026-03-09 14:38:25 -0700

    debugging and more testing

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,4 +1,5 @@
 from audioFuncs import *
+import pyttsx3
 
 import whisper
 
@@ -7,4 +8,7 @@ model = whisper.load_model("base")
 print("Text is:"+record_transcribe(model))
 
 
-
+engine = pyttsx3.init()
+engine.setProperty('rate', 125)
+compSpeak(engine,"") # this clear some lag
+compSpeak(engine,"Hello")
