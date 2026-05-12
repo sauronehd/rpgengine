0036ff3015253827c08bc16f959339b731697956
averageMTGplayer <152660249+sauronehd@users.noreply.github.com>
2026-05-12 12:44:22 -0700

    Using value

--- testingGrounds.py
+++ testingGrounds.py
@@ -3,7 +3,7 @@ from pinCalls import *
 pinOutSet()
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.output(pins.fan, GPIO.HIGH)
+GPIO.output(pins.fan.value, GPIO.HIGH)
 
 wait = input("Press Enter to continue...")
 GPIO.cleanup()
