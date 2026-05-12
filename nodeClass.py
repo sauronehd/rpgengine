823ce22f94633fe67dd25f3b17dfe407a2197e47
averageMTGplayer <152660249+sauronehd@users.noreply.github.com>
2026-05-12 12:42:47 -0700

    Simple testinggrounds output

--- nodeClass.py
+++ nodeClass.py
@@ -14,3 +14,30 @@ class outputNode:
 
     def add_prompt(self, newprompt: str):
         prompt = newprompt
+
+
+class outputNodeTest:
+    prompt = ""
+    outputs = []
+    children = {}
+
+
+
+    def add_child(self, keys: [list[str],'outputNode']):
+        for key in keys[0]:
+            print(f"key is: {key}")
+            self.children[key] = keys[1]
+
+    def add_prompt(self, newprompt: str):
+        prompt = newprompt
+
+
+
+start = outputNodeTest()
+start.add_prompt("Hello")
+fire = outputNodeTest()
+water = outputNodeTest()
+try:
+    start.add_child([["Fire","Left","Heat"],fire])
+finally:
+    print(start.children)
--- testingGrounds.py
+++ testingGrounds.py
@@ -1,25 +1,9 @@
-class outputNodeTest:
-    prompt = ""
-    outputs = []
-    children = {}
+from pinCalls import *
 
+pinOutSet()
+GPIO.setmode(GPIO.BOARD)
 
+GPIO.output(pinOutSet.fan, GPIO.HIGH)
 
-    def add_child(self, keys: [list[str],'outputNode']):
-        for key in keys[0]:
-            print(f"key is: {key}")
-            self.children[key] = keys[1]
-
-    def add_prompt(self, newprompt: str):
-        prompt = newprompt
-
-
-
-start = outputNodeTest()
-start.add_prompt("Hello")
-fire = outputNodeTest()
-water = outputNodeTest()
-try:
-    start.add_child([["Fire","Left","Heat"],fire])
-finally:
-    print(start.children)
+wait = input("Press Enter to continue...")
+GPIO.cleanup()
