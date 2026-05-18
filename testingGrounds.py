f199fc9ee913724f4102bab9997c2b3ba5347c24
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-05-18 13:24:12 -0700

    High not low

--- testingGrounds.py
+++ testingGrounds.py
@@ -4,7 +4,7 @@ pinOutSet()
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.output(pins.fan.value, GPIO.HIGH)
-GPIO.output(pins.heat.value, GPIO.LOW)
+GPIO.output(pins.heat.value, GPIO.HIGH)
 GPIO.output(pins.cold.value, GPIO.HIGH)
 print("Writing to 19, 13, 15")
 wait = input("Press Enter to continue...")
