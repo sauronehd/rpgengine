f8c5e7e62ee7223dc0319569f5c34bb4d8011927
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-05-23 05:34:52 -0700

    reodered code

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,9 +1,9 @@
 from audioFuncs import *
 import pyttsx3
-compSpeak(engine,"")
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 print(voices)
+compSpeak(engine,"")
 engine.setProperty('voice', voices[69].id)
 engine.setProperty('rate', 125)
 compSpeak(engine,"Hello it's me")
