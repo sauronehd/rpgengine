471fd0bd93b07640ae1341ad830601b9fc29a8c5
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-05-26 14:51:17 -0700

    channel # change

--- testingGrounds.py
+++ testingGrounds.py
@@ -7,12 +7,5 @@ from audioFuncs import *
 sd.default.device = (1, 1)
 sd.default.samplerate = 48000
 
-# Play a simple tone
-duration = 2
-freq = 440
-t = np.linspace(0, duration, int(48000 * duration))
-tone = (np.sin(2 * np.pi * freq * t) * 0.3).astype('float32')
-
-sd.play(tone, samplerate=48000, device=1)
 record_transcribe(model)
 sd.wait()
