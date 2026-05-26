36c5b7e514b80cba7ac5c62ef181fd6ed35b522f
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-05-26 15:06:35 -0700

    checking the audio inpput test

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,11 +1,11 @@
-import whisper
-model = whisper.load_model("tiny")
-
 import sounddevice as sd
 import numpy as np
-from audioFuncs import *
+
 sd.default.device = (1, 1)
 sd.default.samplerate = 48000
 
-record_transcribe(model)
+audio = sd.rec(int(3 * 48000), channels=2, dtype='float32')
 sd.wait()
+
+print("Max amplitude:", np.max(np.abs(audio)))
+print("Mean amplitude:", np.mean(np.abs(audio)))
