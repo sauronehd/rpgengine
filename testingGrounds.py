95cedb7f4589e27484d5d92c6871ed30b8010d01
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-05-27 08:40:24 -0700

    bug fix

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,5 +1,7 @@
 from audioFuncs import *
 sd.default.device = (1,1)
+engine = pyttsx3.init()
+
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[69].id)
 engine.setProperty('rate', 125)
