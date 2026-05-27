6d2f7c61bb909a434beebca1c3d97a01eca02e84
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-05-27 08:46:49 -0700

    testing grounds rework

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,10 +1,17 @@
 from audioFuncs import *
 import  pyttsx3
-sd.default.device.output = (1,1)
-engine = pyttsx3.init()
 
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[69].id)
-engine.setProperty('rate', 125)
-compSpeak(engine,"")
-compSpeak(engine, "Hello World")
+import sounddevice as sd
+
+# Find device by name instead of index
+def get_device_by_name(name):
+    for i, dev in enumerate(sd.query_devices()):
+        if name.lower() in dev['name'].lower():
+            return i
+    return None
+
+device_index = get_device_by_name('Google')
+print(f"Found at index: {device_index}")
+
+sd.default.device = (device_index, device_index)
+sd.default.samplerate = 48000
