9d7e7f7dde3374220b20771c639b55c692b40d0e
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-06-01 14:15:50 -0700

    using device 2 for input

--- mictesting.py
+++ mictesting.py
@@ -3,5 +3,6 @@ import whisper
 model = whisper.load_model("tiny")
 import sounddevice as sd
 print(sd.query_devices())
+sd.default.device = (2,1)
 repsonse =  record_transcribe(model)
 print(repsonse)
