f47fed552ebbfe6f748b2e2d38286cee22c756bc
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-06-05 11:05:25 -0700

    changing again

--- pinwrite.py
+++ pinwrite.py
@@ -5,24 +5,4 @@ GPIO.setmode(GPIO.BOARD)
 pinOutSet()
 outputCall([[pins.heat,pinState.on],[pins.fan,pinState.on],[pins.cold,pinState.on]])
 wait = input("Press Enter to continue...")
-print("Tryong all pins now")
 
-newpins = []
-
-for i in range(1,40):
-    newpins.append(i)
-print(newpins)
-
-for pin in newpins:
-    try:
-        GPIO.setup(pin,GPIO.OUT)
-    except Exception as e:
-        print(f"Error on pin {pin}: {e}")
-for pin in newpins:
-    try:
-        GPIO.output(pin, GPIO.OUT)
-    except:
-        print(f"Error on pin {pin}")
-wait = input("Press Enter to continue...")
-
-GPIO.cleanup()
