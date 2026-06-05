4ec2a9dd0d66497e949501017ba310cf2806b7c7
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-06-05 10:33:53 -0700

    fixed arguements again

--- pinwrite.py
+++ pinwrite.py
@@ -3,8 +3,7 @@ from pinCalls import *
 print("Starting")
 GPIO.setmode(GPIO.BOARD)
 pinOutSet()
-for pin in pins:
-    outputCall([pin,pinState.on])
+outputCall([[pins.heat,pinState.on],[pins.fan,pinState.on],[pins.cold,pinState.on]])
 wait = input("Press Enter to continue...")
 GPIO.cleanup()
 print("Goodbye")
