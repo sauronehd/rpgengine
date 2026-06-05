d82e973c346c167254bdbe3323036bcea9fb7f97
lostCoder <152660249+sauronehd@users.noreply.github.com>
2026-06-05 10:25:44 -0700

    1 channel, new device

--- audioFuncs.py
+++ audioFuncs.py
@@ -31,7 +31,7 @@ def record_transcribe(model):
     print("Recording...")
     audio = sd.rec(int(duration * sample_rate),
                    samplerate=sample_rate,
-                   channels=2,
+                   channels=1,
                    dtype='float32')
     sd.wait()
     print("Recording finished")
--- mictesting.py
+++ mictesting.py
@@ -3,7 +3,7 @@ import whisper
 model = whisper.load_model("tiny")
 import sounddevice as sd
 print(sd.query_devices())
-sd.default.device = (2,1)
+sd.default.device = (3,1)
 repsonse =  record_transcribe(model)
 print(repsonse)
 #forcre psy
